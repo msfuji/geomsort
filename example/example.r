c84f77575a5c319acabb88cf2c547cf874d890eb
Masashi Fujita <mssfjt@gmail.com>
2017-02-21 10:10:15 +0900

    updated examples.

--- example/example.r
+++ example/example.r
@@ -1,14 +1,16 @@
-library(geomsort)
 library(ggplot2)
+library(geomsort)
 
 n=300
-df=data.frame(y=c(rnorm(n,1), rnorm(n,2), rnorm(n,3)),
-              x=rep(c("hoge","huga","moge"), each=100),
-              c=rep(c(rep("A",30), rep("B", 30), rep("C", 40)), 3))
+col=c(rep("A",n*0.3), rep("B", n-(n*0.3)))
+df=data.frame(x=rep(c("X","Y","Z"), each=n),
+              y=rnorm(3*n, mean=rep(c(-2, 0, 2), each=n), 4),
+              c=rep(col,3))
 
+print(df)
 p=ggplot(df, aes(x=x, y=y, colour=c))+
-  #geom_point(position=position_sort(width=0.5))
+  geom_point(position=position_sort(width=0.5))
   #geom_point(position=position_sort())
-  geom_sort(width=.75)
-#plot(p)
-ggsave(file="example.png")
+#  geom_sort(width=.75)
+plot(p)
+#ggsave(file="example.png")
