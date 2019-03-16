dc4c245730aed5652301583a28e7a977e4f956e9
Masashi Fujita <mssfjt@gmail.com>
2019-03-17 01:41:53 +0900

    smaller points

--- example/example.r
+++ example/example.r
@@ -2,7 +2,7 @@ library(ggplot2)
 library(geomsort)
 
 p <- ggplot(iris, aes(x=Species, y=Sepal.Length)) + 
-  geom_sort(size=1) +
+  geom_sort(size=.3, pch=1) +
   stat_summary(fun.y = "median", geom = "crossbar", color="red",
                mapping = aes(ymin = ..y.., ymax = ..y..), size=0.3, width=0.3) +
   theme_grey(base_size = 8) + 
