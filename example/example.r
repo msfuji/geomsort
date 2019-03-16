4b39d75f97c21420ac57bea2c8311e610ead9ec9
Masashi Fujita <mssfjt@gmail.com>
2019-03-17 01:30:09 +0900

    use iris for examples

--- example/example.r
+++ example/example.r
@@ -1,15 +1,11 @@
 library(ggplot2)
 library(geomsort)
 
-n=100
-df=data.frame(x=rep(c("X","Y","Z"), each=n),
-              y=rnorm(3*n, mean=rep(c(-2, 0, 2), each=n)))
+p <- ggplot(iris, aes(x=Species, y=Sepal.Length)) + 
+  geom_sort(size=1) +
+  stat_summary(fun.y = "median", geom = "crossbar", color="red",
+               mapping = aes(ymin = ..y.., ymax = ..y..), size=0.3, width=0.3) +
+  theme_grey(base_size = 8) + 
+  xlab(NULL)
 
-p=ggplot(df, aes(x=x, y=y)) + geom_sort(width=0.5, size=0.1) +
-    stat_summary(fun.y = "median", geom = "crossbar", color="red",
-                 mapping = aes(ymin = ..y.., ymax = ..y..), size=0.1, width=0.3)
-p=p + theme_grey(base_size = 7) + xlab(NULL) + ylab("Value")
-
-
-#plot(p, scale=0.3)
-ggsave(file="example.png", plot=p, scale=0.3)
+ggsave(file="example/example.png",  width=2.5, height=2)
--- example/example2.r
+++ example/example2.r
@@ -1,16 +1,9 @@
 library(ggplot2)
 library(geomsort)
 
-n=100
-n3=as.integer(n*0.3)
-col=c(rep("A",n3), rep("B", n-n3))
-df=data.frame(x=rep(c("X","Y","Z"), each=n),
-              y=rnorm(3*n, mean=rep(c(-2, 0, 2), each=n)),
-              c=rep(col,3))
+p <- ggplot(iris, aes(x=Species, y=Sepal.Length, color=Sepal.Width > 3)) + 
+  geom_sort(size=1) +
+  theme_grey(base_size = 8) + 
+  xlab(NULL)
 
-p=ggplot(df, aes(x=x, y=y, colour=c)) + geom_sort(width=0.5, size=0.1)
-p=p + theme_grey(base_size = 7) + xlab(NULL) + ylab("Value")
-
-
-#plot(p, scale=0.3)
-ggsave(file="example2.png", plot=p, scale=0.3)
+ggsave(file="example/example2.png", width=4, height=2)
