84af54bd2de9471d4fdc1ff404b1b20360b60630
Masashi Fujita <mssfjt@gmail.com>
2019-03-20 01:21:55 +0900

    alternating background

--- example/example.r
+++ example/example.r
@@ -1,11 +1,19 @@
 library(ggplot2)
 library(geomsort)
 
-p <- ggplot(iris, aes(x=Species, y=Sepal.Length)) + 
-  geom_sort(size=.3, pch=1) +
-  stat_summary(fun.y = "median", geom = "crossbar", color="red",
-               mapping = aes(ymin = ..y.., ymax = ..y..), size=0.3, width=0.3) +
-  theme_grey(base_size = 8) + 
-  xlab(NULL)
+set.seed(123)
+
+n <- 100
+df <- data.frame(x=rep(c("A", "B", "C", "D", "E", "F"), each=n),
+                 y=rnorm(6*n, mean=rep(c(0:5), each=n)))
+
+p <- ggplot() +
+  geom_tile(data=data.frame(x=unique(df$x)),
+            aes(x=x, y=0, width=1, height=Inf, fill=as.character(as.numeric(x) %% 2)), alpha=0.2) +
+  scale_fill_manual(values = c("0"="white", "1"="blue"), guide="none") +
+  geom_sort(data=df, aes(x=x, y=y), size=0.1)  +
+  stat_summary(data=df, aes(x=as.numeric(x), y=y), fun.y = "median", geom = "point", pch="-", size=7, color="red") +
+  theme_classic(base_size = 7) +
+  labs(x=NULL, y="Value")
 
 ggsave(file="example/example.png",  width=2.5, height=2)
--- example/example2.r
+++
@@ -1,9 +0,0 @@
-library(ggplot2)
-library(geomsort)
-
-p <- ggplot(iris, aes(x=Species, y=Sepal.Length, color=Sepal.Width > 3)) + 
-  geom_sort(size=1) +
-  theme_grey(base_size = 8) + 
-  xlab(NULL)
-
-ggsave(file="example/example2.png", width=4, height=2)
